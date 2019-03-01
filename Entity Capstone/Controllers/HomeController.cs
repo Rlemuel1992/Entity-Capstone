6c96a8affd596f44c070818ba0cab92ab09a8578
Ronald Lemuel <Rlemuel1992@gmail.com>
2019-03-01 15:07:01 -0500

    First commit

---
+++ Entity Capstone/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Entity_Capstone.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+
+			return View();
+		}
+
+		public ActionResult Courses()
+		{
+			return View();
+		}
+
+		public ActionResult Java()
+		{
+			return View();
+		}
+
+		public ActionResult CSharp()
+		{
+			return View();
+		}
+
+		public ActionResult FrontEnd()
+		{
+			return View();
+		}
+		[Authorize]
+		public ActionResult SignUp()
+		{
+			return View();
+		}
+
+	}
+}
---
+++ Entity Capstone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Entity_Capstone.Startup))]
+namespace Entity_Capstone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
